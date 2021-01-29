e1938d1926b55045c60d77e4e2aba5e5d7007eb5
Emir <emirsavas@windowslive.com>
2021-01-29 20:47:30 +0300

    Interact Objects

---
+++ Assets/Scripts/InteractableObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObjects : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction, Color.blue);
+        RaycastHit raycastHit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (Physics.Raycast(ray, out raycastHit, Mathf.Infinity))
+            {
+                Debug.Log(raycastHit.collider.name);
+            }
+        }
+    }
+}
