b359574d73921eb5e078386fa5c41976c5f70eab
onur <onur-yesiltas@hotmail.com>
2021-01-31 14:40:44 +0300

    changes

---
+++ Assets/Scripts/Ui.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Ui : MonoBehaviour
+{
+   
+
+    public string levelName;
+
+    
+
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+      
+    }
+
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+}
